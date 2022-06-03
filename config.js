b31347df155d913f583df99a4ef4d239bc312aa5
quockhai99 <106815473+quockhai99@users.noreply.github.com>
2022-06-03 19:57:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "cuadedanhyeumeoleonheo",
+    introTitle: 'của để dành',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao tấm lại yêu tui nha <3',
+    btnReply: 'Gửi cho yêu thương <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'hì quốc biết mà 🥰. Yêu thương nhiều nhiều 😘😘',
+    messDesc: 'Tấm ơi,làm vợ tuii nhaaa ,,trả lời thật lòng nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/tieuho155' //
 }
