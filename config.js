9bb0fba6028a94ca3b9c10919b2d0ddd7dd6deae
quockhai99 <106815473+quockhai99@users.noreply.github.com>
2022-06-03 20:06:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     titleWeb: "cuadedanhyeumeoleonheo",
     introTitle: 'của để dành',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Đố ai quét lá sạch rừng?
+    Đố ai khuyên được anh ngừng yêu em
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'yêu anh quốc của em <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao tấm lại yêu tui nha <3',
     btnReply: 'Gửi cho yêu thương <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'hì quốc biết mà 🥰. Yêu thương nhiều nhiều 😘😘',
+    reply: 'vì quốc là người mà em yêu nhất nhà nha <33333333',
+    mess: 'hì quốc biết mà 🥰. Yêu thương vợ nhiều nhiều 😘😘',
     messDesc: 'Tấm ơi,làm vợ tuii nhaaa ,,trả lời thật lòng nhá.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/tieuho155' //
